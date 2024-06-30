246113e395a0b9fa4ce411bb3c0a44ac1f75364f
coder_ <satvikbhatnagar420@gmail.com>
2024-06-30 20:23:27 +0000

    load data from website
    Signed-off-by: coder_ <satvikbhatnagar420@gmail.com>

--- web_scraping/webscraping.py
+++ web_scraping/webscraping.py
@@ -9,3 +9,9 @@ table_name = 'Top_50'
 csv_path = '/home/project/top_50_films.csv'
 df = pd.DataFrame(columns=["Average Rank","Film","Year"])
 count = 0
+
+html_page = requests.get(url).text
+data = BeautifulSoup(html_page, 'html.parser')
+
+tables = data.find_all('tbody')
+rows = tables[0].find_all('tr')
