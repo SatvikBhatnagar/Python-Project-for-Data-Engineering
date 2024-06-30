5b5f18577c35f1690526820afc7dfc77add43812
coder_ <satvikbhatnagar420@gmail.com>
2024-06-30 23:21:55 +0000

    print the table from the DB to console and append value

--- SQLite3/db_code.py
+++ SQLite3/db_code.py
@@ -11,4 +11,27 @@ file_path = './INSTRUCTOR.csv'
 df = pd.read_csv(file_path, names = attribute_list)
 
 df.to_sql(table_name, conn, if_exists = 'replace', index =False)
-print('Table is ready')
+
+query_statement = f"SELECT * FROM {table_name}"
+query_output = pd.read_sql(query_statement, conn)
+print(query_statement)
+print(query_output)
+def count_db():
+    query_statement = f"SELECT COUNT(*) FROM {table_name}"
+    query_output = pd.read_sql(query_statement, conn)
+
+    print(query_statement)
+    print(query_output)
+
+count_db()
+
+data_dict = {'ID' : [100],
+            'FNAME' : ['John'],
+            'LNAME' : ['Doe'],
+            'CITY' : ['Paris'],
+            'CCODE' : ['FR']}
+data_append = pd.DataFrame(data_dict)
+data_append.to_sql(table_name, conn, if_exists = 'append', index =False)
+print('Data appended successfully')
+
+count_db()
